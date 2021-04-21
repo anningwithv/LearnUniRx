6b6d07973a7d35eb4629818050eb2b0eb568b1d5
zhangwei_modooplay <wei.zhang@modooplay.com>
2021-04-21 18:33:08 +0800

    update

--- LearnUnirx/Assets/Demo/UIBind/UIBindTestPanel.cs
+++ LearnUnirx/Assets/Demo/UIBind/UIBindTestPanel.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UniRx;
+using UniRx.Triggers;
 
 public class UIBindTestPanel : MonoBehaviour
 {
     [SerializeField] private Button m_Btn1;
     [SerializeField] private InputField m_InputField1;
     [SerializeField] private Text m_Text1;
+    [SerializeField] private Image m_Image1;
 
     private void Start()
     {
@@ -18,6 +20,7 @@ public class UIBindTestPanel : MonoBehaviour
 
         //m_InputField1.OnEndEditAsObservable().SubscribeToText(m_Text1);
         m_InputField1.OnValueChangedAsObservable().SubscribeToText(m_Text1);
-       
+
+        m_Image1.OnBeginDragAsObservable().Subscribe(_ => { Debug.Log("On image dragged"); });
     }
 }
